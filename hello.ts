6ce193878c61eb3f5e3e8b375cf7041f0b1cf4a3
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-06 20:18:44 +0530

    typescript

---
+++ hello.ts
@@ -0,0 +1,11 @@
+// Introduce Types to javascript  ----- Javascript with types = Typescript
+
+let x:number|string = 30
+let fname:string|null = "Kushal"
+
+x = "Kushal"
+fname = null
+
+console.log(x);
+console.log(fname);
+
